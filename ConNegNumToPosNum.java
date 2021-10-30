1e44304c3dc44eea176a2b5ad72ebbfd45a7361e
sakthepriyaRK <93398174+sakthepriyaRK@users.noreply.github.com>
2021-10-30 21:05:35 +0530

    Add files via upload

---
+++ ConNegNumToPosNum.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class ConNegNumToPosNum 
+{
+	public static void main(String[] args) 
+	{
+		int num=-40;
+		int N;
+		N=num+(2*(-num));
+	
+		System.out.println("Number coverted to a positive number: "+ N );
+		
+		 if(num<0)
+		{
+			System.out.println("Number is a negative number");
+			
+			
+			
+	}
+		else {
+			System.out.println("Number is a Positive number");
+		}
+		}
+	}
+	
+
+
